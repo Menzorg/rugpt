62e679c5e723d9f6ffdafd5538c4cd3189c5dda6
pe4enushko <mr.stone9999@gmail.com>
2026-02-12 13:03:34 +0400

    logging is not print lol

--- src/engine/agents/tools/rag_tool.py
+++ src/engine/agents/tools/rag_tool.py
@@ -24,7 +24,7 @@ def rag_search(query: str, collection: str = "") -> str:
     emb = OllamaEmbeddings(model='nomic-embed-text-v2-moe:latest', base_url='http://localhost:11434')
     client = PGVector(connection=Config.get_vector_dsn(),embeddings=emb,collection_name=collection)
     
-    logger.info(f"rag_search called: query={query}, collection={collection}")
+    logger.info(f"rag_search called: query={query} collection={collection}")
     
     docs = client.similarity_search(query, k = 6)
     scored_docs = []
@@ -36,11 +36,11 @@ def rag_search(query: str, collection: str = "") -> str:
             score += 1 if keyword in doc.page_content else 0
         scored_docs.append((score, doc))
     
-    logging.info("Scored:", [score for score, doc in scored_docs])
+    logging.info(f"Scored: {[score for score, doc in scored_docs]}")
     scored_docs.sort(key = lambda x: x[0], reverse=True)
     top_docs = scored_docs[0:3]
 
-    logging.info("Top chosen from hybrid search:", [score for score, doc in top_docs])
+    logging.info(f"Top chosen from hybrid search: {[score for score, doc in top_docs]}")
 
     serialized = "\n\n".join((f"Score: {score}\nDocument: {doc.page_content}\nMetadata: {doc.metadata}\n") for score, doc in top_docs)
     return serialized
